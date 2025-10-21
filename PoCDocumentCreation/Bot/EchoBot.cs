babdc11ff50ecf80c29586e5f4db63d5fcb48a9a
mgoyeneche <mgoyenbecheb@unal.edu.co>
2025-10-21 15:00:45 -0500

    Agregar archivos de proyecto.

---
+++ PoCDocumentCreation/Bot/EchoBot.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Agents.Builder;
+using Microsoft.Agents.Builder.App;
+using Microsoft.Agents.Builder.State;
+using Microsoft.Agents.Core.Models;
+
+namespace PoCDocumentCreation.Bot;
+
+public class EchoBot : AgentApplication
+{
+    public EchoBot(AgentApplicationOptions options) : base(options)
+    {
+        OnConversationUpdate(ConversationUpdateEvents.MembersAdded, WelcomeMessageAsync);
+
+        // Listen for ANY message to be received. MUST BE AFTER ANY OTHER MESSAGE HANDLERS
+        OnActivity(ActivityTypes.Message, OnMessageAsync);
+    }
+
+    protected async Task WelcomeMessageAsync(ITurnContext turnContext, ITurnState turnState, CancellationToken cancellationToken)
+    {
+        foreach (ChannelAccount member in turnContext.Activity.MembersAdded)
+        {
+            if (member.Id != turnContext.Activity.Recipient.Id)
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text("Hello and Welcome!"), cancellationToken);
+            }
+        }
+    }
+
+    protected async Task OnMessageAsync(ITurnContext turnContext, ITurnState turnState, CancellationToken cancellationToken)
+    {
+        await turnContext.SendActivityAsync($"You said: {turnContext.Activity.Text}", cancellationToken: cancellationToken);
+    }
+}
